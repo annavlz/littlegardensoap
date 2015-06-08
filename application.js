ab1d96fadfa7ffda4a03e79ae941f05c031cc747
Anna Lezhikova <anna.v.l@gmail.com>
2015-06-08 18:09:45 +1200

    add mouseleave to gallery

--- application.js
+++ application.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
 	$('td').on('mouseenter', function() {
 		$(this).closest('td').find('span').slideToggle();
 	});
+	$('td').on('mouseleave', function() {
+		$(this).closest('td').find('span').slideToggle();
+	});
 });
